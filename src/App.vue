8023bb6acf91ad720357726ec324a7d582c4291f
Lucjan Wiśnicz <luqapo@gmail.com>
2019-02-17 10:07:04 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -17,9 +17,11 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 10px;
   a {
+    padding: 0 30px;
     font-weight: bold;
+    text-decoration: none;
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
